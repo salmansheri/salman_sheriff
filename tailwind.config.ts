16720b4afa816cc140a522cd356799e7887bb542
salmansheri <sheriffsalman00@gmail.com>
2024-09-20 21:05:57 +0530

    feat: enhance UI with new components, animations, and styles
    1. Added dependencies: clsx, lucide-react, and tailwind-merge to enhance
    component and styling capabilities.
    2. Updated global color palette and commented out dark mode for future
    use.
    3. Replaced static content on the homepage with a reusable Hero
    component.
    4. Enhanced header with new buttons for navigation and interaction.
    5. Introduced new animations (spin-around, slide) and keyframes in
    Tailwind configuration to enable dynamic UI elements.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,8 @@ const config: Config = {
       animation: {
         gradient: "gradient 8s linear infinite",
         rainbow: "rainbow var(--speed, 2s) infinite linear",
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
+        slide: "slide var(--speed) ease-in-out infinite alternate",
       },
       keyframes: {
         rainbow: {
@@ -29,6 +31,25 @@ const config: Config = {
         gradient: {
           to: {
             backgroundPosition: "var(--bg-size) 0",
+          },
+        },
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+        slide: {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
           },
         },
       },
