4b99cbfae7a864d01d44603b134d493e93645bf1
salmansheri <sheriffsalman00@gmail.com>
2024-09-20 19:01:21 +0530

    feat: Implement Hero Section

---
+++ src/components/header.tsx
@@ -0,0 +1,35 @@
+const navLinks = [
+  {
+    id: 1,
+    title: "Home",
+    href: "#",
+  },
+  {
+    id: 2,
+    title: "Projects",
+    href: "#projects",
+  },
+  {
+    id: 3,
+    title: "About",
+    href: "#about",
+  },
+  {
+    id: 4,
+    title: "Contact",
+    href: "#contact",
+  },
+];
+export const Header = () => {
+  return (
+    <header className="flex justify-center items-center relative top-3">
+      <nav className="flex gap-1 p-0.5 border items-center border-white/15 rounded-full bg-white/10 backdrop-blur ">
+        {navLinks.map((link) => (
+          <a className="nav-item" href={link.href} key={link.id}>
+            {link.title}
+          </a>
+        ))}
+      </nav>
+    </header>
+  );
+};
